11f3b4b01814b070de4bbc06dbbddcd1eeec5344
mahmutsel <mahmutsel29@gmail.com>
2021-01-13 23:24:22 +0000

    Express server setup with initial routes

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+
+//Define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`The app is listening to ${PORT}`));
