2660e292474a042b65e9a9dc01f999f4a8885db4
mahmutsel <mahmutsel29@gmail.com>
2021-01-15 15:19:23 +0000

    registration completed with JWT

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
-
 const app = express();
+const connectDB = require('./config/db');
 
+connectDB();
 
+app.use(express.json({ extended: false }));
 
-app.get('/',(req,res)=>{
-    res.send('Hello World')
-})
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
 //Define routes
 app.use('/api/users', require('./routes/users'));
