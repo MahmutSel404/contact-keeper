c92898604989f9ee786d776c181b67a3c5418dfe
mahmutsel <mahmutsel29@gmail.com>
2021-01-16 23:15:35 +0000

    contact form created

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Contacts from '../contacts/Contacts';
+import ContactForm from '../contacts/ContactForm';
+import ContactState from '../../context/Contact/ContactState';
 
 const Home = () => {
   return (
     <div className='grid-2'>
-      <div>{/* ContactForm */}</div>
+      <div>
+        <ContactForm />{' '}
+      </div>
       <div>
         <Contacts />
       </div>
